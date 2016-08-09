2c27cf910ef5dff2a5c9bc25804cb54f3e53c33a
apatronl <apl7@gatech.edu>
2016-08-09 11:10:35 -0700

    Added SearchView

---
+++ Crawl/app/src/main/java/com/codeu/team51/crawl/SearchActivity.java
@@ -0,0 +1,54 @@
+package com.codeu.team51.crawl;
+
+import android.app.SearchManager;
+import android.content.Context;
+import android.support.v4.view.MenuItemCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.ListView;
+import android.support.v7.widget.SearchView;
+
+public class SearchActivity extends AppCompatActivity {
+
+    private ListView resultsView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        resultsView = (ListView) findViewById(R.id.resultsView);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_search, menu);
+        MenuItem searchItem = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Log.d("Crawl", "Search!");
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String searchQuery) {
+                return false;
+            }
+        });
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        return id == R.id.action_search || super.onOptionsItemSelected(item);
+    }
+
+}
