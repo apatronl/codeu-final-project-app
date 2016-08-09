f8fa66f24539015e96dacca5a4aef59bdab9df82
apatronl <apl7@gatech.edu>
2016-08-09 13:39:38 -0700

    Added hard-coded listview

--- Crawl/app/src/main/java/com/codeu/team51/crawl/SearchActivity.java
+++ Crawl/app/src/main/java/com/codeu/team51/crawl/SearchActivity.java
@@ -1,19 +1,25 @@
 package com.codeu.team51.crawl;
 
-import android.app.SearchManager;
-import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.support.v7.widget.SearchView;
+
+import java.util.ArrayList;
 
 public class SearchActivity extends AppCompatActivity {
 
     private ListView resultsView;
+    private ArrayList<String> urls = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,19 +27,48 @@ public class SearchActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         resultsView = (ListView) findViewById(R.id.resultsView);
+
+        resultsView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.d("Crawl", "Item clicked!");
+                // TODO: get actual url of item pressed
+                Intent browserIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("http://www.google.com"));
+                startActivity(browserIntent);
+            }
+        });
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.menu_search, menu);
         MenuItem searchItem = menu.findItem(R.id.action_search);
-        SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+        final SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+
+
+        searchView.setOnQueryTextFocusChangeListener(new View.OnFocusChangeListener() {
+            @Override
+            public void onFocusChange(View v, boolean hasFocus) {
+
+            }
+        });
 
         searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-
             @Override
             public boolean onQueryTextSubmit(String query) {
                 Log.d("Crawl", "Search!");
+                urls.add("url1");
+                urls.add("url2");
+                urls.add("url3");
+                urls.add("url4");
+                urls.add("url5");
+
+                ArrayAdapter<String> adapter =
+                        new ArrayAdapter<String>(SearchActivity.this,
+                                android.R.layout.simple_list_item_1, urls);
+
+                resultsView.setAdapter(adapter);
+                searchView.clearFocus();
                 return true;
             }
 
